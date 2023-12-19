95708bde9fbf8aac2cc94b9cf8b301702dc3854d
JavaBrewer <hks0658@gmail.com>
2023-12-19 17:46:42 +0900

    2

---
+++ src/main/java/com/example/temoboard/TemoboardApplication.java
@@ -0,0 +1,13 @@
+package com.example.temoboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemoboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemoboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/temoboard/TemoboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.temoboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemoboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
